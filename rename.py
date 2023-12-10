acc75d107d100e42d7faa0eedbf0219559c5fd4f
Rizwan Ahmad <rizwan.bhatti@maanz-ai.com>
2023-12-10 21:38:10 +0500

    rename script needed since expandnet script appends a suffix

---
+++ rename.py
@@ -0,0 +1,19 @@
+import os
+import glob
+from tqdm import tqdm
+
+hdrRoot = os.path.join(os.getcwd(), 'resources', 'hdr-yolo-coco-person')
+hdrTrain = os.path.join(hdrRoot, 'train', 'images')
+hdrValidation = os.path.join(hdrRoot, 'train', 'validation')
+
+hdrTrainFilePaths = glob.glob(os.path.join(hdrTrain, '*'))
+hdrValidationFilePaths = glob.glob(os.path.join(hdrValidation, '*'))
+
+paths = list()
+# paths.extend(hdrTrainFilePaths)
+paths.extend(hdrValidationFilePaths)
+
+for p in tqdm(paths):
+    name = p.split('_prediction')[0]
+    ext = p.split('.')[-1]
+    os.rename(p, name+'.'+ext)
