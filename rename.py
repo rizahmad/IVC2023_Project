9a6a1f5fd4f0726b09e5587a4590758123471f22
Rizwan Ahmad <rizwan.bhatti@maanz-ai.com>
2023-12-10 21:51:10 +0500

    commenting out execution loop to prevent mistakes

--- rename.py
+++ rename.py
@@ -4,16 +4,16 @@ from tqdm import tqdm
 
 hdrRoot = os.path.join(os.getcwd(), 'resources', 'hdr-yolo-coco-person')
 hdrTrain = os.path.join(hdrRoot, 'train', 'images')
-hdrValidation = os.path.join(hdrRoot, 'train', 'validation')
+hdrValidation = os.path.join(hdrRoot, 'validation', 'images')
 
 hdrTrainFilePaths = glob.glob(os.path.join(hdrTrain, '*'))
 hdrValidationFilePaths = glob.glob(os.path.join(hdrValidation, '*'))
 
 paths = list()
-# paths.extend(hdrTrainFilePaths)
+paths.extend(hdrTrainFilePaths)
 paths.extend(hdrValidationFilePaths)
 
-for p in tqdm(paths):
-    name = p.split('_prediction')[0]
-    ext = p.split('.')[-1]
-    os.rename(p, name+'.'+ext)
+# for p in tqdm(paths):
+#     name = p.split('_prediction')[0]
+#     ext = p.split('.')[-1]
+#     os.rename(p, name+'.'+ext)
